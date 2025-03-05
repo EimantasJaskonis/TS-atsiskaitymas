55a651d399a686864f7bec1c8950fa48894555a1
EimantasJaskonis <eimantasjaskonis@gmail.com>
2025-03-05 09:21:16 +0200

    Task2

--- Task 2/script.ts
+++ Task 2/script.ts
@@ -2,43 +2,55 @@
 Aprašykite type'us, kad kintamieji, kuriems jie priskirti būtų teisingai aprašyti ir visas kodas veiktų teisingai.
 Kur komentare parašyta "error", ta eilutė po tipo priskyrimo kintamąjam turėtų mesti klaidą. Pasitikrinus užkomentuoti visą eilutę, kad leistų sukompiliuoti.-------------------------------------------------------------------------- */
 type One = {
-
-}
-let one: One = {
+  vardas: string;
+  pavarde: string;
+  amzius: number;
+  };
+  
+  let one2: One = {
   vardas: '',
   pavarde: '',
   amzius: 0
-};
-type Two = {
-
-}
-let two: Two = {
+  };
+  
+  type Two = {
+  vardas: string;
+  pavarde: string;
+  amzius: number;
+  [key: string]: any; 
+  };
+  
+  let two2: Two = {
   vardas: '',
   pavarde: '',
   amzius: 0
-};
-two = {
+  };
+  two2 = {
   vardas: '',
   pavarde: '',
   amzius: 0,
   ugis: 0,
   svoris: 0
-};
-type Three = {
-
-}
-let three: Three = {
+  };
+  
+  type Three = {
+  vardas: string;
+  pavarde: string;
+  lytis: 'vyras' | 'moteris';
+  };
+  
+  let three2: Three = {
   vardas: '',
   pavarde: '',
   lytis: 'vyras'
-};
-three = {
+  };
+  three2 = {
   vardas: '',
   pavarde: '',
   lytis: 'moteris'
-};
-three = {
-  vardas: '',
-  pavarde: '',
-  lytis: 'sraigtasparnis' // error
-};
+  };
+  // three2 = {
+  // vardas: '',
+  // pavarde: '',
+  // // lytis: 'sraigtasparnis' // error
+  // };
